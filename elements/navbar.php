7a21d105e6de29907476fa752dfa1e27c725a6fe
squick <ramzi.idir@proton.me>
2025-05-22 16:38:28 +0200

    remake de la nav

--- elements/navbar.php
+++ elements/navbar.php
@@ -9,8 +9,8 @@
             <li><a href="?page=3" class="cta">Nulle</a></li>
             <li><a href="?page=4" class="cta">Pratique</a></li>
             <li><a href="?page=5" class="cta">Utilisations</a></li>
-            <?php include "./elements/content/loginpopup.php"?>
         </ul>
+        <?php include "./elements/content/loginpopup.php"?>
     </div>
 
     <div id="menu_mobile">
@@ -21,6 +21,7 @@
         <span></span>
 
         <ul id="menu">
+            <li><?php include "./elements/content/loginpopup.php"?></li>
             <li><a href="?page=0" class="cta">Définitions</a></li>
             <li><a href="?page=1" class="cta">Positif</a></li>
             <li><a href="?page=2" class="cta">Négatif</a></li>
